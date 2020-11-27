1972cf4d95182ce4ac3152d66ce9e0fa922f7c2f
Illia Shakin <S00188372@mail.itsligo.ie>
2020-11-27 15:44:29 +0000

    Added Classes

---
+++ Program/Song.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class Song
+    {
+
+        public string Title { get; set; }
+
+        public string Artist { get; set; }
+
+        public double Duration { get; set; }
+
+        public Genre MusicGenre { get; set; }
+
+    }
+
+    public enum Genre{Rock, Pop,Dance, Other }
+}
